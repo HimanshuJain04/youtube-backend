3c97ba8290af106ceb43da7c549600bdd277dd2d
Himanshu <himanshujain5690174@gmail.com>
2024-02-01 15:39:28 +0530

    Set Up Complete

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// import 
 import express from "express";
 import http from "http";
 import bodyParser from "body-parser";
@@ -6,12 +7,27 @@ import cors from "cors";
 import compression from "compression";
 
 
-
+// variables
 const app = express();
+const PORT = 4000;
 
+// Middlewares
 app.use(cors({
     credentials: true,
 }));
+app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(compression());
+
+
+// server listen
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log("Server listening on port: ", PORT)
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Default Route");
+});
